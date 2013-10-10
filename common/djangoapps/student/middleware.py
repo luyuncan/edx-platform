3ccb6277cbb852b893bd3f3041743520d9895eba
Adam Palay <adam@edx.org>
2013-10-10 11:16:41 -0400

    add middleware

---
+++ common/djangoapps/student/middleware.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
+class UserStandingMiddleware(object):
+	"""
+	Checks a user's standing on request. Deletes their session if the user's
+	status is 'disabled'.
+	"""
+	def process_request(self, request):
+		student = request.user
+		if student.standing.account_status = u'account_disabled':
+			request.session.flush()
+			return redirect(reverse('login'))
