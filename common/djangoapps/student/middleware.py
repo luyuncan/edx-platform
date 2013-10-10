9bf0399257c09ace48954cb50455d8135bc5fbbc
Adam Palay <adam@edx.org>
2013-10-10 11:40:17 -0400

    refactor middleware

--- common/djangoapps/student/middleware.py
+++ common/djangoapps/student/middleware.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
+from student.models import UserStanding
 
 class UserStandingMiddleware(object):
 	"""
@@ -7,7 +8,14 @@ class UserStandingMiddleware(object):
 	status is 'disabled'.
 	"""
 	def process_request(self, request):
-		student = request.user
-		if student.standing.account_status == u'account_disabled':
-			request.session.flush()
-			return redirect(reverse('login'))
+		user = request.user
+		try:
+			user_account = UserStanding.objects.get(user=user)
+			# because user is a unique field in UserStanding, there will either be
+			# one or zero user_accounts associated with a UserStanding
+		except UserStanding.DoesNotExist:
+			pass
+		else:
+			if user_account.account_standing == u'account_disabled':
+				request.session.flush()
+				return redirect(reverse('login'))
